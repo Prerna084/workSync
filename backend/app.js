240d548f71793128b18526c87f7f70557252d965
Prerna <prerna084pandey@gmail.com>
2026-05-24 22:58:02 +0530

    final cors setup

--- backend/app.js
+++ backend/app.js
@@ -1,36 +1,34 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 
-const authRoutes = require('./src/routes/authRoutes');
-const taskRoutes = require('./src/routes/taskRoutes');
-const inviteRoutes = require('./src/routes/inviteRoutes');
-const setupSwagger = require('./src/config/swagger');
+const authRoutes = require("./src/routes/authRoutes");
+const taskRoutes = require("./src/routes/taskRoutes");
+const inviteRoutes = require("./src/routes/inviteRoutes");
+const setupSwagger = require("./src/config/swagger");
 
 const app = express();
 
-app.use(cors({
-  origin: [
-    "http://localhost:5173",
-    "https://worksync-frontend-7uj1.onrender.com"
-  ],
-  methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
-  credentials: true
-}));
-
-
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://worksync-frontend-7uj1.onrender.com",
+    ],
+    credentials: true,
+  })
+);
 
 app.use(express.json());
 
-app.use('/api/auth', authRoutes);
-app.use('/api/tasks', taskRoutes);
-app.use('/api/invites', inviteRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/tasks", taskRoutes);
+app.use("/api/invites", inviteRoutes);
 
 // Initialize Swagger Documentation
 setupSwagger(app);
 
-// General simple route check
-app.get('/', (req, res) => {
-  res.send('WorkSync API is running');
+app.get("/", (req, res) => {
+  res.send("Backend running");
 });
 
 module.exports = app;
